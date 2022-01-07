3639b4b9c1d110cdd0f056c0b8484f1eb1f134d4
Henk Kelder <h.kelder@pxs.com>
2022-01-07 09:46:18 +0100

    Angular12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', loadChildren: './home/home.module#HomePageModule'},
-  {path: 'avatars', loadChildren: './avatars/avatars.module#AvatarsPageModule'},
-  {path: 'cards', loadChildren: './cards/cards.module#CardsPageModule'},
-  {path: 'concurrency', loadChildren: './concurrency/concurrency.module#ConcurrencyPageModule'},
-  {path: 'ng-content', loadChildren: './ng-content/ng-content.module#NgContentPageModule'},
-  {path: 'slider', loadChildren: './slider/slider.module#SliderPageModule'},
-  {path: 'virtual-scroll', loadChildren: './virtual-scroll/virtual-scroll.module#VirtualScrollPageModule'},
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)},
+  {path: 'avatars', loadChildren: () => import('./avatars/avatars.module').then(m => m.AvatarsPageModule)},
+  {path: 'cards', loadChildren: () => import('./cards/cards.module').then(m => m.CardsPageModule)},
+  {path: 'concurrency', loadChildren: () => import('./concurrency/concurrency.module').then(m => m.ConcurrencyPageModule)},
+  {path: 'ng-content', loadChildren: () => import('./ng-content/ng-content.module').then(m => m.NgContentPageModule)},
+  {path: 'slider', loadChildren: () => import('./slider/slider.module').then(m => m.SliderPageModule)},
+  {path: 'virtual-scroll', loadChildren: () => import('./virtual-scroll/virtual-scroll.module').then(m => m.VirtualScrollPageModule)},
 ];
 
 @NgModule({
