425821e430c2c15b99cd41c4cca1d1ed18d242da
Henk Kelder <h.kelder@pxs.com>
2024-10-09 21:25:32 +0200

    Angular18

--- projects/ionic-image-loader/src/lib/ionic-image-loader.module.ts
+++ projects/ionic-image-loader/src/lib/ionic-image-loader.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 // import { File } from '@awesome-cordova-plugins/file/ngx';
 import { IonicModule } from '@ionic/angular';
@@ -8,15 +8,9 @@ import { IonicImageLoaderComponent } from './ionic-image-loader.component';
 import { ImageLoaderConfigService } from './services/image-loader-config.service';
 import { ImageLoaderService } from './services/image-loader.service';
 
-@NgModule({
-  imports: [
-    IonicModule,
-    HttpClientModule,
-    CommonModule,
-  ],
-  declarations: [IonicImageLoaderComponent],
-  exports: [IonicImageLoaderComponent],
-})
+@NgModule({ declarations: [IonicImageLoaderComponent],
+    exports: [IonicImageLoaderComponent], imports: [IonicModule,
+        CommonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class IonicImageLoader {
   static forRoot(): ModuleWithProviders<IonicImageLoader> {
     return {
